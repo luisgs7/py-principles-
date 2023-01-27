e9c3948efc5a704f2ff9424f1f24969c833569cb
luisgs7 <luisgsilva270@gmail.com>
2023-01-27 15:25:53 -0300

    new class motobike polymorphism

---
+++ poo/motorbike_poli.py
@@ -0,0 +1,56 @@
+from motorbike_int import IMotorbike
+
+
+class NewMoto(IMotorbike):
+    def __init__(self, nome) -> None:
+        self.__nome = nome
+
+    def start(self) -> None:
+        print("start")
+    
+    def stop(self) -> None:
+        print("No break!!!")
+    
+    @property
+    def nome(self) -> str:
+        return self.__nome
+
+    @nome.setter
+    def nome(self, valor: str) -> None:
+        self.__nome = valor
+
+
+class OldMoto(IMotorbike):
+    def __init__(self, nome) -> None:
+        self.__nome = nome
+
+    def start(self) -> None:
+        print("start")
+    
+    def stop(self) -> None:
+        print("Stopping")
+    
+    @property
+    def nome(self) -> str:
+        return self.__nome
+
+    @nome.setter
+    def nome(self, valor: str) -> None:
+        self.__nome = valor
+
+
+if __name__ == "__main__":
+    nwm = NewMoto("Moto 2024")
+
+    print(nwm.nome)
+    nwm.nome
+    nwm.start()
+    nwm.stop()
+
+    print()
+    
+    old = OldMoto("Moto 1975")
+
+    print(old.nome)
+    old.start()
+    old.stop()
